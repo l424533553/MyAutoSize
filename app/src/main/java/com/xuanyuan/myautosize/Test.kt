7677a88cd660fd23528186f863bd7dbff8e7d7d8
罗发新 <luofaxin@seaeverit.com>
2020-11-11 14:53:48 +0800

    2020.11.11

---
+++ app/src/main/java/com/xuanyuan/myautosize/Test.kt
@@ -0,0 +1,15 @@
+package com.xuanyuan.myautosize
+
+/**
+ * @author 罗发新
+ * 创建时间：2020/11/11  13:54
+ * 邮件：424533553@qq.com
+ * CSDN：https://blog.csdn.net/luo_boke
+ *
+ * 更新时间：2020/11/11  13:54
+ *
+ *文件说明：
+ *
+ */
+class Test {
+}
